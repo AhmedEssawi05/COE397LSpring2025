f729690025b91ab8f287e4f2e23db2d961ca565d
Ahmed Essawi <157429597+AhmedEssawi05@users.noreply.github.com>
2025-01-24 22:40:33 -0600

    Add files via upload

---
+++ homework1/build/CMakeFiles/commrank.c.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/commrank.c.dir/commrank.c.o"
+  "CMakeFiles/commrank.c.dir/commrank.c.o.d"
+  "commrank.c"
+  "commrank.c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/commrank.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
